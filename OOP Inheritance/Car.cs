a4a3cd07f33e44b5941d07f99af58aed11650989
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-05-01 10:17:05 +0500

    Add project files.

---
+++ OOP Inheritance/Car.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace OOP_Inheritance
+{
+    internal abstract class Car
+    {
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Car(string model, decimal price)
+        {
+            Model = model;
+            Price = price;
+        }
+
+        public abstract void Drive();
+    }
+
+    internal class ExpensiveCar : Car
+    {
+        public virtual double Battery { get; set; }
+
+        public ExpensiveCar(string model, decimal price, double battery)
+            : base(model, price)
+        {
+            Battery = battery;
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine($"Driving the {Model} expensive car.");
+        }
+
+        public virtual void Stop()
+        {
+            Console.WriteLine($"Expensive car {Model} is stopping");
+        }
+    }
+    
+
+    internal class Tesla : ExpensiveCar
+    {
+        public int TeslaVersion { get; set; }
+
+        public Tesla(string model, decimal price, double battery, int teslaVersion)
+            : base(model, price, battery)
+        {
+            TeslaVersion = teslaVersion;
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine($"Driving the {Model} Tesla car.");
+        }
+    }
+
+    internal class TeslaZ : Tesla
+    {
+        public virtual int ZNumber { get; set; }
+
+        public TeslaZ(string model,
+            decimal price,
+            double battery,
+            int version,
+            int number)
+            : base(model, price, battery, version)
+        {
+            ZNumber = number;
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine($"Driving the {Model} Tesla Z car.");
+        }
+    }
+
+    internal class TeslaX : Tesla
+    {
+        public virtual int XNumber { get; set; }
+
+        public TeslaX(string model,
+            decimal price,
+            double battery,
+            int version,
+            int number)
+            : base(model, price, battery, version)
+        {
+            XNumber = number;
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine($"Driving the {Model} Tesla X car.");
+        }
+    }
+}
---
+++ OOP Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP_Inheritance
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+          
+            TeslaZ teslaZ = new TeslaZ("Model Z", 100000m, 75.5, 3, 42);
+           
+            teslaZ.Drive();
+
+            teslaZ.Stop();
+ 
+            Console.WriteLine($"Tesla Z version: {teslaZ.TeslaVersion}");
+            Console.WriteLine($"Tesla Z number: {teslaZ.ZNumber}");
+ 
+            TeslaX teslaX = new TeslaX("Model X", 150000m, 100.0, 4, 21);
+   
+            teslaX.Drive();
+  
+            Console.WriteLine($"Tesla X version: {teslaX.TeslaVersion}");
+            Console.WriteLine($"Tesla X number: {teslaX.XNumber}");
+
+            Car[] cars = new Car[2];
+            cars[0] = new TeslaZ("Model Z", 100000m, 75.5, 3, 42);
+            cars[1] = new TeslaX("Model X", 150000m, 100.0, 4, 21);
+
+            foreach (Car car in cars)
+            {
+                car.Drive();
+            }
+        }
+    }
+}
